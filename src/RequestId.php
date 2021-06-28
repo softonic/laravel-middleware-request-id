ca392671e1c22facdca34ff30bcef8221d2fe21b
joskfg <joskfg@gmail.com>
2021-06-28 16:58:22 +0200

    Set X-Request-ID to server globals (#4)
    Set X-Request-ID to server globals

--- src/RequestId.php
+++ src/RequestId.php
@@ -24,10 +24,14 @@ class RequestId
     public function handle(Request $request, \Closure $next, $guard = null)
     {
         $uuid = $request->headers->get('X-Request-ID');
+
         if (is_null($uuid)) {
             $uuid = Uuid::uuid4()->toString();
+
             $request->headers->set('X-Request-ID', $uuid);
         }
+
+        $_SERVER['HTTP_X_REQUEST_ID'] = $uuid;
 
         $response = $next($request);
 
